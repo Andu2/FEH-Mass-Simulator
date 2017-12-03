c96171e1797705a13bd1a4c7faecfb3e78ac3e68
Jason Chen <jyc8@live.com>
2017-12-03 11:48:59 -0800

    Update seals SP in db and add to total SPT

--- code.js
+++ code.js
@@ -917,16 +917,18 @@ function setStats(hero){
 			mergeBoost.res -= 2;
 		}
 		
+		hero.hp += mergeBoost.hp;
 		hero.atk += mergeBoost.atk;
 		hero.spd += mergeBoost.spd;
 		hero.def += mergeBoost.def;
 		hero.res += mergeBoost.res;
 		
-		//Add hero hp changes
-		hero.hp += mergeBoost.hp + hero.buffs.hp + hero.debuffs.hp;
-	
+		//Add hero hp changes - from blessings
+		hero.hp += hero.buffs.hp + hero.debuffs.hp;
+		
 		//Calculate hero bst after IV, merge, and rarity
-		hero.bst = hero.hp + hero.atk + hero.spd + hero.def + hero.res;
+		//TODO: Include blessing stat changes
+		hero.bst = hero.hp + hero.atk + hero.spd + hero.def + hero.res;	
 
 		//Add stats based on skills
 		if(hero.weapon != -1){
@@ -1049,6 +1051,7 @@ function updateSpt(hero){
 	hero.spt += (hero.a != -1 ? data.skills[hero.a].sp : 0);
 	hero.spt += (hero.b != -1 ? data.skills[hero.b].sp : 0);
 	hero.spt += (hero.c != -1 ? data.skills[hero.c].sp : 0);
+	hero.spt += (hero.s != -1 ? data.skills[hero.s].sp : 0);
 }
 
 function setSkills(hero){
