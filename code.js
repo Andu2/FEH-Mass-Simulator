a62aa5ad6e0735a4a9aa0770adb1651330e0b10d
Jason Chen <jyc8@live.com>
2018-01-14 21:34:26 -0800

    Add rarity and IVs to base values in tooltip for #66

--- code.js
+++ code.js
@@ -2218,19 +2218,97 @@ function showHeroTooltip(heroType){
 			break;
 	}
 	
-	if (hero != -1 && hero.index != -1){
-		showingTooltip = true;
-		
-		tooltipText = "<span class=\"bold\">" + data.heroes[hero.index].name + "</span>";
-		tooltipText += " ☆☆☆☆☆" + "<br>";
-		tooltipText += " Base HP: <font color=\"#fefec8\">" + data.heroes[hero.index].basehp + "</font><br>";
-		tooltipText += " Base Atk: <font color=\"#fefec8\">" + data.heroes[hero.index].baseatk + "</font><br>";
-		tooltipText += " Base Spd: <font color=\"#fefec8\">" + data.heroes[hero.index].basespd + "</font><br>";
-		tooltipText += " Base Def: <font color=\"#fefec8\">" + data.heroes[hero.index].basedef + "</font><br>";
-		tooltipText += " Base Res: <font color=\"#fefec8\">" + data.heroes[hero.index].baseres + "</font><br>";
-
-		$("#frame_tooltip").html(tooltipText).show();
+	//If hero is undefined or empty, do nothing
+	if (hero == -1 || hero.index == -1){
+		return;
 	}
+	
+	//Show tooltip
+	showingTooltip = true;
+	var base = {};
+	base.hp = data.heroes[hero.index].basehp;
+	base.atk = data.heroes[hero.index].baseatk;
+	base.spd = data.heroes[hero.index].basespd;
+	base.def = data.heroes[hero.index].basedef;
+	base.res = data.heroes[hero.index].baseres;
+	
+	if (hero.rarity < 5){
+		var rarityBaseOrder = ["atk","spd","def","res"];
+		var boostPriority = {"hp":4,"atk":3,"spd":2,"def":1,"res":0};
+		rarityBaseOrder.sort(function(a,b){
+			if(base[a]>base[b]){
+				return -1;
+			}
+			else if(base[a]<base[b]){
+				return 1;
+			}
+			else{
+				if(boostPriority[a]>boostPriority[b]){
+					return -1;
+				}
+				else{
+					return 1;
+				}
+			}
+		});
+
+		rarityBaseOrder.push("hp");
+		var rarityBoostCount = Math.floor((hero.rarity-1) * 2.5);
+		
+		//Subtract 2 from every stat to revert 5* base stats to 1*
+		//***Shouldn't this be done before sorting rarityBaseOrder?***
+		base.hp -= 2;
+		base.atk -= 2;
+		base.spd -= 2;
+		base.def -= 2;
+		base.res -= 2;
+		
+		//Add bonus to 1* base stats to rarity values
+		for(var i = 0; i < rarityBoostCount; i++){
+			base[rarityBaseOrder[i%5]]++;
+		}
+	}
+	
+	//Add IV stats
+	var growthValMod = {"hp":0,"atk":0,"spd":0,"def":0,"res":0};
+	if(hero.boon != "none"){
+		growthValMod[hero.boon] += 1;
+	}
+	if(hero.bane != "none"){
+		growthValMod[hero.bane] -= 1;
+	}	
+	base.hp += growthValMod.hp;
+	base.atk += growthValMod.atk;
+	base.spd += growthValMod.spd;
+	base.def += growthValMod.def;
+	base.res += growthValMod.res;
+	
+	//Print tooltip text
+	tooltipText = "<span class=\"bold\">" + data.heroes[hero.index].name + " (";	
+	if ((hero.boon == "none" && hero.bane == "none") || hero.boon == hero.bane){
+		tooltipText += "Neutral";
+	}else{
+		//Print boon first
+		for (var stat in growthValMod){
+			if (growthValMod.hasOwnProperty(stat) && growthValMod[stat] > 0) {
+				tooltipText += "+" + stat;
+			}
+		}
+		//Print bane second
+		for (var stat in growthValMod){
+			if (growthValMod.hasOwnProperty(stat) && growthValMod[stat] < 0) {
+				tooltipText += "-" + stat;
+			}
+		}
+	}	
+	tooltipText += ") " + "☆".repeat(hero.rarity) + " </span><br>";
+	tooltipText += " Base HP: <font color=\"#fefec8\">" + base.hp + "</font><br>";
+	tooltipText += " Base Atk: <font color=\"#fefec8\">" + base.atk + "</font><br>";
+	tooltipText += " Base Spd: <font color=\"#fefec8\">" + base.spd + "</font><br>";
+	tooltipText += " Base Def: <font color=\"#fefec8\">" + base.def + "</font><br>";
+	tooltipText += " Base Res: <font color=\"#fefec8\">" + base.res + "</font><br>";
+
+	$("#frame_tooltip").html(tooltipText).show();
 }
 
 function showSkillTooltip(heroType, skillType){
